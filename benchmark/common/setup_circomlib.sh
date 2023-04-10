c69c7a0d65fc864901d8c7e9e7d586818b0e2c28
Ron Kahat <ron.kahat@gmail.com>
2023-04-11 01:11:08 +0300

    fix: fetch script dir

--- benchmark/common/setup_circomlib.sh
+++ benchmark/common/setup_circomlib.sh
@@ -2,7 +2,9 @@
 
 CIRCOMLIB_REPO_URL="https://github.com/iden3/circomlib"
 
-script_dir=$(realpath "$0")
+script=$(realpath "$0")
+script_dir=$(dirname "$script")
+
 target_dir="$script_dir/circomlib"
 
 git clone "$CIRCOMLIB_REPO_URL" $target_dir
